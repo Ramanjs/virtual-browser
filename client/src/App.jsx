d70c970ce18dad177cf8b37d50e964ce82254399
Ramanjs <raman.s2662@gmail.com>
2023-10-28 04:37:06 +0530

    render image data from backend to canvas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,36 @@
+import {useEffect, useRef, useState} from "react";
+import {io} from "socket.io-client";
+
 function App() {
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas.getContext('2d')
+    console.log(canvas)
+    console.log(ctx)
+
+    const socket = io("http://localhost:8080")
+    socket.on('connect', () => {
+      console.log(socket.id)
+    })
+
+    socket.on('image', (data) => {
+      //console.log(data)
+
+      let image = new Image()
+      image.src = URL.createObjectURL(new Blob([data])) 
+      image.onload = () => {
+        ctx.drawImage(image, 0, 0)
+      }
+    })
+  }, [])
 
   return (
     <>
       <div>
         <h1>Hello world</h1>
+        <canvas ref={canvasRef} width={600} height={400}></canvas>
       </div>
     </>
   )
