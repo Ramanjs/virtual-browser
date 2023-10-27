6f832a072e7d60cd16d5124012465540b06e4859
Ramanjs <raman.s2662@gmail.com>
2023-10-28 04:36:33 +0530

    allow cors and optimize screenshot

--- server/server.ts
+++ server/server.ts
@@ -8,42 +8,54 @@ import { Server } from 'socket.io'
 import puppeteer from 'puppeteer'
 
 dotenv.config()
-
-const app = express()
 const logger = morgan('dev')
 
+const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
 
 app.use(logger)
 app.use(errorHandler)
 
 app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server')
+  res.json('Express + TypeScript Server')
 })
 
 export const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+})
 
 io.on('connection', async (socket) => {
+  console.log(socket.id)
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
 
   await page.goto('https://google.com')
-  await page.setViewport({ width: 1080, height: 1024 })
+  await page.setViewport({ width: 600, height: 400 })
+  await page.keyboard.press('Space')
 
   const intervalId = setInterval(() => {
-    page.screenshot()
+    page.screenshot({
+      optimizeForSpeed: true,
+      type: 'jpeg'
+    })
       .then(data => {
         socket.emit('image', data)
       })
       .catch(err => {
         console.log(err)
       })
-  }, 20)
+  }, 50)
 
-  socket.on('disconnect', () => {
+  socket.on('disconnect', async () => {
     clearInterval(intervalId)
+    await page.close()
+    await browser.close()
   })
 })
