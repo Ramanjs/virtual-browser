813df4a22c144f0a094c4f29b3532ec003ff662b
Ramanjs <raman.s2662@gmail.com>
2023-10-28 05:30:17 +0530

    receive frames inorder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,34 +3,33 @@ import {io} from "socket.io-client";
 
 function App() {
   const canvasRef = useRef(null)
+  const socket = io("http://localhost:8080")
 
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
-    console.log(canvas)
-    console.log(ctx)
+    let imageUrl = ''
 
-    const socket = io("http://localhost:8080")
     socket.on('connect', () => {
-      console.log(socket.id)
-    })
 
-    socket.on('image', (data) => {
-      //console.log(data)
-
-      let image = new Image()
-      image.src = URL.createObjectURL(new Blob([data])) 
-      image.onload = () => {
-        ctx.drawImage(image, 0, 0)
-      }
+      socket.on('image', (data) => {
+        URL.revokeObjectURL(imageUrl)
+        let image = new Image()
+        imageUrl = URL.createObjectURL(new Blob([data])) 
+        image.src = imageUrl
+        image.onload = () => {
+          ctx.drawImage(image, 0, 0)
+          socket.emit('getimg')
+        }
+      })
     })
-  }, [])
+  }, [socket])
 
   return (
     <>
       <div>
         <h1>Hello world</h1>
-        <canvas ref={canvasRef} width={600} height={400}></canvas>
+        <canvas ref={canvasRef} width={800} height={600}></canvas>
       </div>
     </>
   )
